a93e5236af9a1dd49859660f6796d3afb262be0d
Naveen-Dubba <Naveen-Dubba@users.noreply.github.com>
2026-06-23 05:48:12 +0000

    deploy: 6485108910bbc6c7dec7c329fb3dded983b2215c

--- main.dart.js
+++ main.dart.js
@@ -29684,7 +29684,7 @@ $S:2}
 A.azQ.prototype={
 $1(a){var s=A.dm().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:115}
 A.O_.prototype={
 gv(a){var s=this.a
